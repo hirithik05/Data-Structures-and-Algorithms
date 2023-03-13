d262011627f73e38bf68a6976b23736941000c7d
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-03-13 11:00:30 +0530

    Add files via upload

---
+++ Basic/Program3.java
@@ -0,0 +1,42 @@
+package java_tutorial;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		// following are just decleare
+		byte a = 100;
+		short b = 5000;
+		int c = 5;
+		
+		//Some modification need in this data type during declaration
+		long d = 404343434L;
+		float e = 5.9f;
+		double f = 5.55d;
+		
+		//Boolean in java
+		boolean g = true;
+		boolean h = false;
+		
+		// character in java
+		char i = 'B';
+		char j = 65 ;
+		
+		//String in java
+		String k = "Ritik";
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		
+
+	}
+
+}
