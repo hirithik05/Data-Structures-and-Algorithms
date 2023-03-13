8fd040f38af88ef4f681c248bff7bd95ba6f81ab
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-03-13 11:00:05 +0530

    Add files via upload

---
+++ Basic/Program2.java
@@ -0,0 +1,12 @@
+package java_tutorial;
+
+public class Program2 {
+	public static void main(String[] args) {
+		System.out.println("Ritik Kumar Verma");
+		//This is a single line comment in java
+		/* This is a multi line comment in java
+		 Hey Ritik
+		 */
+	}
+
+}
