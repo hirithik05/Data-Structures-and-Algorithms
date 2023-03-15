bd620f0ae71bc535930783ab01dc4ba96b980d25
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-03-16 00:32:38 +0530

    Add files via upload

---
+++ Basic/Relational.java
@@ -0,0 +1,18 @@
+package java_tutorial;
+import java.util.Scanner;
+
+public class Relational {
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.print("Enter first number :");
+		int num1 = obj.nextInt();
+		System.out.print("Enter second number :");
+		int num2 = obj.nextInt();
+		System.out.println(num1==num2);
+		System.out.println(num1>num2);
+		System.out.println(num1<num2);
+		System.out.println(num1>=num2);
+		System.out.println(num1<=num2);
+	}
+
+}
