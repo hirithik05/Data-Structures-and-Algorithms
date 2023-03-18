69f116cb8ccd13f89ecf9da234b4036293c23994
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-03-18 11:59:29 +0530

    Add files via upload

--- Array/LinearSearch.java
+++ Array/LinearSearch.java
@@ -1,1 +1,20 @@
+package Array;
+import java.util.Scanner;
 
+public class LinearSearch {
+	int linear(int[] arr,int X) {
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==X) {
+				return i;
+			}
+			
+		}
+			return -1;
+	}
+	public static void main(String[] args) {
+		int[] arr1 = {2,4,6,7,9};
+		LinearSearch obj = new LinearSearch();
+		System.out.println(obj.linear(arr1, 4));
+	}
+
+}
