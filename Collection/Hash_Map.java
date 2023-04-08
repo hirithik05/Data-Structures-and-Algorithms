d46668890e87e2b4abe48cb9e79735f3036d3016
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-04-08 16:27:48 +0530

    Add files via upload

---
+++ Collection/Hash_Map.java
@@ -0,0 +1,16 @@
+package Collections;
+
+import java.util.HashMap;
+
+public class Hash_Map {
+	public static void main(String[] args) {
+		HashMap<Integer,String>obj = new HashMap();
+		//Add element as key-pair
+		obj.put(1, "Ritik");
+		obj.put(2, "Ankit");
+		obj.put(3, "Vivek");
+		obj.size();
+		System.out.println(obj);
+	}
+
+}
