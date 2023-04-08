b0a5a3cd2837eace60591ffa90ca1cd459a62d50
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-04-08 16:37:23 +0530

    Add files via upload

---
+++ Collection/Tree_Set.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.TreeSet;
+
+public class Tree_Set {
+	public static void main(String[] args) {
+		TreeSet<Integer>obj = new TreeSet();
+		//Add element in set
+		obj.add(4);
+		obj.add(1);
+		obj.add(7);
+		obj.add(8);
+		System.out.println(obj);
+		
+		
+	}
+
+}
