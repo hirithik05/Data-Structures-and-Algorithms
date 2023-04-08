939efb933af435f09587727bb928efe9c805115a
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-04-08 16:13:29 +0530

    Add files via upload

---
+++ Collection/LinkedList_.java
@@ -0,0 +1,22 @@
+package Collections;
+
+import java.util.LinkedList;
+
+public class LinkedList_ {
+	public static void main(String[] args) {
+		LinkedList<Integer>obj = new LinkedList<>();
+		//Add element in Linked list
+		obj.add(4);
+		obj.add(5);
+		obj.add(1);
+		obj.add(9);
+		obj.add(42);
+		//delete element in LinkedList
+		obj.remove(2);
+		obj.size();
+		System.out.println(obj);
+		
+		
+	}
+
+}
