ed696055e53ea0e0f01b2944d61774e5de57492c
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-04-08 16:30:37 +0530

    Add files via upload

---
+++ Collection/Hash_Set.java
@@ -0,0 +1,16 @@
+package Collections;
+
+import java.util.HashSet;
+
+public class Hash_Set {
+	public static void main(String[] args) {
+		HashSet<Integer>obj = new HashSet();
+		//Add element in Set
+		obj.add(3);
+		obj.add(5);
+		obj.add(2);
+		obj.add(12);
+		System.out.println(obj);
+	}
+
+}
