8968f9056eaa1f6d4c11cf5fc1be54efc32ece55
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-04-08 16:43:28 +0530

    Add files via upload

---
+++ Collection/Tree_Map.java
@@ -0,0 +1,17 @@
+package Collections;
+
+import java.util.TreeMap;
+
+public class Tree_Map {
+	public static void main(String[] args) {
+		TreeMap<Integer,String>obj = new TreeMap();
+		obj.put(1, "Ritik");
+		obj.put(8, "Ankit");
+		obj.put(2, "Vivek");
+		obj.put(3, "Aditya");
+		System.out.println(obj);
+		
+		
+	}
+
+}
