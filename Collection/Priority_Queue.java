e285fdf118e158af88681926f5260b45d0a39549
Ritik Kumar Verma <106764626+hirithik05@users.noreply.github.com>
2023-04-08 16:18:13 +0530

    Add files via upload

---
+++ Collection/Priority_Queue.java
@@ -0,0 +1,17 @@
+package Collections;
+
+import java.util.PriorityQueue;
+
+public class Priority_Queue {
+	public static void main(String[] args) {
+		PriorityQueue<Integer> obj = new PriorityQueue();
+		obj.offer(45);
+		obj.offer(4);
+		obj.offer(5);
+		obj.offer(9);
+		System.out.println(obj);
+		
+		
+	}
+
+}
